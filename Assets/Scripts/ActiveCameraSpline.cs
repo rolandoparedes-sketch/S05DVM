e94445aa14c8c9937394168456f567fe27f26c0f
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2026-04-18 23:54:07 -0500

    poa

--- Assets/Scripts/ActiveCameraSpline.cs
+++ Assets/Scripts/ActiveCameraSpline.cs
@@ -5,41 +5,53 @@ public class ActivarCamaraSpline : MonoBehaviour
 {
     public CinemachineCamera camaraNormal;
     public CinemachineCamera camaraSpline;
-    public CinemachineSplineDolly dolly;
 
+    private CinemachineSplineDolly dolly;
     private bool enSpline = false;
+    private bool yaSeActivo = false;
+    public float velocidad = 0.2f; 
 
     void Start()
     {
         camaraNormal.Priority.Value = 10;
         camaraSpline.Priority.Value = 0;
 
-        dolly.enabled = false; 
+        dolly = camaraSpline.GetComponent<CinemachineSplineDolly>();
+        dolly.CameraPosition = 0f;
+        dolly.enabled = false;
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.CompareTag("Cesped") && !enSpline)
+        if (hit.gameObject.CompareTag("Cesped") && !enSpline && !yaSeActivo)
         {
-            Debug.Log("FIFA17");
-
             camaraNormal.Priority.Value = 0;
             camaraSpline.Priority.Value = 20;
 
-            dolly.enabled = true;         
-            dolly.CameraPosition = 0f;   
+            dolly.enabled = true;
+            dolly.CameraPosition = 0f;
 
             enSpline = true;
+            yaSeActivo = true; 
+        }
+    }
 
-            Invoke("VolverCamara", 5f); 
+    void Update()
+    {
+        if (enSpline)
+        {
+            dolly.CameraPosition += Time.deltaTime * 0.2f;
+
+            if (dolly.CameraPosition >= 1f)
+            {
+                VolverCamara();
+            }
         }
     }
 
     void VolverCamara()
     {
-        Debug.Log("A JUGAR");
-
-        dolly.enabled = false; 
+        dolly.enabled = false;
 
         camaraSpline.Priority.Value = 0;
         camaraNormal.Priority.Value = 50;
